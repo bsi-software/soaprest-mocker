ad8852e9fb6d3f4a00b5ebf3d4e023daed0131cc
Jacek Obrymski <jaceko@onet.pl>
2012-11-18 18:53:41 +0100

    adding licence information

--- soaprest-mock-service/src/main/java/net/sf/jaceko/mock/resource/RestEndpointResource.java
+++ soaprest-mock-service/src/main/java/net/sf/jaceko/mock/resource/RestEndpointResource.java
@@ -1,3 +1,22 @@
+/**
+ *
+ *     Copyright (C) 2012 Jacek Obarymski
+ *
+ *     This file is part of SOAP/REST Mock Servce.
+ *
+ *     SOAP/REST Mock Servce is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License, version 3
+ *     as published by the Free Software Foundation.
+ *
+ *     SOAP/REST Mock Servce is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with SOAP/REST Mock Servce; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package net.sf.jaceko.mock.resource;
 
 import javax.servlet.http.HttpServletRequest;
