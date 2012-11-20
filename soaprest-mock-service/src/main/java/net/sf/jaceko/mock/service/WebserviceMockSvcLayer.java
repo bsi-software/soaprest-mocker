219cb273e65e34680aad32eae58186394170e1a3
Jacek Obrymski <jaceko@onet.pl>
2012-11-20 22:26:23 +0100

    thread safety

--- soaprest-mock-service/src/main/java/net/sf/jaceko/mock/service/WebserviceMockSvcLayer.java
+++ soaprest-mock-service/src/main/java/net/sf/jaceko/mock/service/WebserviceMockSvcLayer.java
@@ -19,6 +19,9 @@
  */
 package net.sf.jaceko.mock.service;
 
+import static java.util.Collections.synchronizedList;
+import static java.util.Collections.synchronizedMap;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -32,15 +35,15 @@ import net.sf.jaceko.mock.exception.ServiceNotConfiguredException;
 import net.sf.jaceko.mock.model.MockResponse;
 import net.sf.jaceko.mock.model.MockRequest;
 
-
 public class WebserviceMockSvcLayer {
 	private MockserviceConfiguration configuration;
 
-	private Map<String, Map<String, Collection<MockRequest>>> recordedRequestsMap = new HashMap<String, Map<String, Collection<MockRequest>>>();
+	private final Map<String, Map<String, Collection<MockRequest>>> recordedRequestsMap = synchronizedMap(new HashMap<String, Map<String, Collection<MockRequest>>>());
 
 	private DelayService delayService;
 
-	public MockResponse performRequest(String serviceName, String operationId, String request, String queryString, String resourceId) {
+	public MockResponse performRequest(String serviceName, String operationId, String request, String queryString,
+			String resourceId) {
 		WebserviceOperation serviceOperation = getWebserviceOperation(serviceName, operationId);
 		int invocationNumber = serviceOperation.getNextInvocationNumber();
 		MockResponse response = serviceOperation.getResponse(invocationNumber);
@@ -50,8 +53,7 @@ public class WebserviceMockSvcLayer {
 
 	}
 
-	public void setCustomResponse(String serviceName, String operationId, int requestInOrder,
-			MockResponse expectedResponse) {
+	public void setCustomResponse(String serviceName, String operationId, int requestInOrder, MockResponse expectedResponse) {
 		WebserviceOperation serviceOperation = getWebserviceOperation(serviceName, operationId);
 		serviceOperation.setCustomResponse(expectedResponse, requestInOrder);
 		serviceOperation.resetInvocationNumber();
@@ -73,27 +75,43 @@ public class WebserviceMockSvcLayer {
 	private WebserviceOperation getWebserviceOperation(String serviceName, String operationId) {
 		WebserviceOperation serviceOperation = configuration.getWebServiceOperation(serviceName, operationId);
 		if (serviceOperation == null) {
-			throw new ServiceNotConfiguredException("Undefined webservice operation: operationId:"
-					+ operationId + " of service: " + serviceName);
+			throw new ServiceNotConfiguredException("Undefined webservice operation: operationId:" + operationId
+					+ " of service: " + serviceName);
 		}
 		return serviceOperation;
 	}
 
 	private void recordRequest(String serviceName, String operationId, String requestBody, String queryString, String resourceId) {
-		Map<String, Collection<MockRequest>> requestsPerOperationMap = recordedRequestsMap.get(serviceName);
-		if (requestsPerOperationMap == null) {
-			requestsPerOperationMap = new HashMap<String, Collection<MockRequest>>();
-			recordedRequestsMap.put(serviceName, requestsPerOperationMap);
-		}
+		Map<String, Collection<MockRequest>> requestsPerOperationMap = fetchRequestsPerOperationMap(serviceName);
 
-		Collection<MockRequest> recordedRequests = requestsPerOperationMap.get(operationId);
-		if (recordedRequests == null) {
-			recordedRequests = new ArrayList<MockRequest>();
-			requestsPerOperationMap.put(operationId, recordedRequests);
-		}
+		Collection<MockRequest> recordedRequests = fetchRecordedRequests(operationId, requestsPerOperationMap);
 
 		MockRequest request = new MockRequest(requestBody, queryString, resourceId);
 		recordedRequests.add(request);
+	}
+
+	private Collection<MockRequest> fetchRecordedRequests(String operationId,
+			Map<String, Collection<MockRequest>> requestsPerOperationMap) {
+
+		synchronized (requestsPerOperationMap) {
+			Collection<MockRequest> recordedRequests = requestsPerOperationMap.get(operationId);
+			if (recordedRequests == null) {
+				recordedRequests = synchronizedList(new ArrayList<MockRequest>());
+				requestsPerOperationMap.put(operationId, recordedRequests);
+			}
+			return recordedRequests;
+		}
+	}
+
+	private synchronized Map<String, Collection<MockRequest>> fetchRequestsPerOperationMap(String serviceName) {
+
+		Map<String, Collection<MockRequest>> requestsPerOperationMap = recordedRequestsMap.get(serviceName);
+		if (requestsPerOperationMap == null) {
+			requestsPerOperationMap = synchronizedMap(new HashMap<String, Collection<MockRequest>>());
+			recordedRequestsMap.put(serviceName, requestsPerOperationMap);
+		}
+
+		return requestsPerOperationMap;
 	}
 
 	public Collection<String> getRecordedRequestBodies(String serviceName, String operationId) {
@@ -129,17 +147,18 @@ public class WebserviceMockSvcLayer {
 		}
 		return recordedRequests;
 	}
-	
+
 	public Collection<String> getRecordedResourceIds(String serviceName, String operationId) {
 		Collection<MockRequest> recordedRequests = getRecordedRequests(serviceName, operationId);
 
 		Collection<String> recordedResourceIds = new ArrayList<String>();
-		for (MockRequest request : recordedRequests) {
-			recordedResourceIds.add(request.getResourceId());
+		synchronized (recordedRequests) {
+			for (MockRequest request : recordedRequests) {
+				recordedResourceIds.add(request.getResourceId());
+			}
 		}
 		return recordedResourceIds;
 	}
-
 
 	public void setDelayService(DelayService delayService) {
 		this.delayService = delayService;
@@ -158,6 +177,5 @@ public class WebserviceMockSvcLayer {
 	private void clearRecordedRequests(String serviceName, String operationId) {
 		getRecordedRequests(serviceName, operationId).clear();
 	}
-
 
 }
