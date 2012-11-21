e2379725f792e63adb2262d36f4f1a04bd1237a2
Jacek Obrymski <jaceko@onet.pl>
2012-11-21 22:30:44 +0100

    thread safety

--- soaprest-mock-service/src/main/java/net/sf/jaceko/mock/service/WebserviceMockSvcLayer.java
+++ soaprest-mock-service/src/main/java/net/sf/jaceko/mock/service/WebserviceMockSvcLayer.java
@@ -103,15 +103,15 @@ public class WebserviceMockSvcLayer {
 		}
 	}
 
-	private synchronized Map<String, Collection<MockRequest>> fetchRequestsPerOperationMap(String serviceName) {
-
-		Map<String, Collection<MockRequest>> requestsPerOperationMap = recordedRequestsMap.get(serviceName);
-		if (requestsPerOperationMap == null) {
-			requestsPerOperationMap = synchronizedMap(new HashMap<String, Collection<MockRequest>>());
-			recordedRequestsMap.put(serviceName, requestsPerOperationMap);
+	private Map<String, Collection<MockRequest>> fetchRequestsPerOperationMap(String serviceName) {
+		synchronized (recordedRequestsMap) {
+			Map<String, Collection<MockRequest>> requestsPerOperationMap = recordedRequestsMap.get(serviceName);
+			if (requestsPerOperationMap == null) {
+				requestsPerOperationMap = synchronizedMap(new HashMap<String, Collection<MockRequest>>());
+				recordedRequestsMap.put(serviceName, requestsPerOperationMap);
+			}
+			return requestsPerOperationMap;
 		}
-
-		return requestsPerOperationMap;
 	}
 
 	public Collection<String> getRecordedRequestBodies(String serviceName, String operationId) {
